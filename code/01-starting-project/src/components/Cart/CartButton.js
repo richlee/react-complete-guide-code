61a129a4d49f7ad4d131f8ab1e8779a24376ad2d
rich <richalee@pm.me>
2022-06-19 14:27:50 +0100

    refresher - redux

--- code/01-starting-project/src/components/Cart/CartButton.js
+++ code/01-starting-project/src/components/Cart/CartButton.js
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+
+  // on click then dispatch the toggle command
+  const dispatch = useDispatch();
+
+  // on load show the cart quantity
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- code/01-starting-project/src/components/Shop/Products.js
+++ code/01-starting-project/src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1', price: 6, title: 'my first book', description: 'first book I wrote'},
+  { id: 'p2', price: 5, title: 'my second  book', description: 'I did it again'}
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map((product) => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
+        ))}
       </ul>
     </section>
   );
