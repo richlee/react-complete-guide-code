05f9abdf1822e2b00c26c0d78f0b14a5c7ca237e
rich <richalee@pm.me>
2022-06-22 15:14:29 +0100

    more flexible routes & http calls

--- code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,34 +1,57 @@
-import { Fragment } from "react";
-import { Route, useParams, Link } from "react-router-dom";
+import { Fragment, useEffect } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Rich", text: "Learning React is fun" },
-  { id: "q2", author: "Ricardo", text: "Spelling correctly is even better" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const {quoteId} = params;
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true); // true to start component in 'pending'
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]); 
+ 
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuote.text) { // api always returns a quote, even if it's 'empty''
+    return <p>Couldn't find quote</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
